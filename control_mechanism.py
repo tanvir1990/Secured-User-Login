ed6178d147f27cfd8966718a902274cd380eb280
tanvir1990 <pavel.mhp@gmail.com>
2020-11-03 00:18:36 -0500

    Added strip to input

--- control_mechanism.py
+++ control_mechanism.py
@@ -4,7 +4,7 @@ def can_access(role):
     f = open('positions.json')
     data = json.load(f)
     for (k) in data:
-        if role == k.get('role_type'):
+        if role.strip() == k.get('role_type').strip():
             return k
             break
 
