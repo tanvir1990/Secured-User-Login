4518dad80d85d7ac0c8ce58a719c6b9030b65f36
tanvir1990 <pavel.mhp@gmail.com>
2020-11-03 00:21:06 -0500

    ignore changes

--- read_json.py
+++ read_json.py
@@ -1,5 +1,5 @@
 import json
-
+import datetime
 # role = "Teller"
 # available_positions = []
 # f = open('positions.json')
@@ -16,19 +16,31 @@ import json
 # print("Available Positions in the system are: ", available_positions)
 
 
-role = "Regular_Client"
-f = open('positions.json')
-data = json.load(f)
-value = {}
-def read_position():
-    for (k) in data:
-        if role == k.get('role_type'):
-            return k.get('operations')
-            break
+# role = "Regular_Client"
+# f = open('positions.json')
+# data = json.load(f)
+# value = {}
+# def read_position():
+#     for (k) in data:
+#         if role == k.get('role_type'):
+#             print(k.get('role_type'))
+#             return k.get("operations")
+#             break
+#
+# f.close()
+#
+# operations = (read_position())
+# print(operations)
+# for (i) in operations:
+#     print(operations[i])
+now = datetime.datetime.now()
+print('Time is ', now)
 
-f.close()
 
-operations = (read_position())
+business_hours_open = now.replace(hour=9, minute=0, second=0, microsecond=0)
+business_hours_close = now.replace(hour=17, minute=0, second=0, microsecond=0)
 
-for (i) in operations:
-    print(operations[i])
+if business_hours_open < now < business_hours_close:
+    print('Valid')
+else:
+    print("Closed")
