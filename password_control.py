7e3010bcf19b779a91aede1d7358f26983d4d03e
tanvir1990 <pavel.mhp@gmail.com>
2020-11-03 01:18:31 -0500

    Added the functions

--- password_control.py
+++ password_control.py
@@ -1,14 +1,22 @@
+# Tanvir Hossain
+# Id 101058988
+# SYSC 4810 Assignment 3
+# Problem 2
 import warnings
-def add_record():
-    return 0
+
+
+def add_record(username_input, position_type, env):
+    role_database = open("role_types.txt", "a")
+    role_database.write(username_input + ' ' + position_type + ' ' + env + "\n")
+    role_database.close()
 
 
 # Reads Record from a "Database" called role_types.txt
 def read_record(user_name):
     password_file = open("role_types.txt")
-    user_list = {}             #One for complete Info
+    user_list = {}             # One for complete Info
     user_env = {}
-    user_array = []            #One for Verification
+    user_array = []            # One for Verification
     for line in password_file:
         record = line.split(' ')
         user = record[0]
@@ -20,11 +28,32 @@ def read_record(user_name):
     password_file.close()
 
     if user_name in user_array:
-        return user_list.get(user_name), user_env.get(user_name),     #Basically retrieves the roletype for the given user
+        # Basically retrieves the roletype for the given user
+        return user_list.get(user_name), user_env.get(user_name),
     else:
         warnings.warn("User name not Found")
 
 
-read_record('Tanvir')
+def read_password(user_name):
+    password_file = open("password.txt")
+    # One for complete Info
+    user_list = {}
+    # One for Verification
+    user_array = []
 
+    for line in password_file:
+        record = line.split(' ')
+        user = record[0]
+        user_array.append(record[0])
+        password = record[1]
+        salt = record[2]
+        user_list.update({user: password})
+        if user == user_name:
+            break
+    password_file.close()
+
+    if user_name in user_array:
+        return user_list.get(user), salt  # Basically retrieves the hashed password for the given user
+    else:
+        warnings.warn("User name not Found")
 
