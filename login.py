a3f1675dce17d0e63780bd279178e32ba3dbadd0
tanvir1990 <pavel.mhp@gmail.com>
2020-11-03 00:35:35 -0500

    Added the features for timing of Teller

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 from Code.control_mechanism import *
 from Code.password_db import *
 import hashlib
+import datetime
 
 #@TODO that Env thing
 #@TODO randomize salt
@@ -23,8 +24,13 @@ def login_verification(user_name, password_input, role, env):
     retrieved_password, retrieved_salt = read_password(user_name.strip())
     decoded_password = hashlib.sha256((str(password_input).strip()
                                        + str(retrieved_salt.strip())).encode()).hexdigest()
+    if int(env) == 1:
+        system_access = check_system_time_and_access()
+    else:
+        system_access = True
+
     # Check the Password
-    if decoded_password.strip() == retrieved_password.strip():
+    if password_is_valid(decoded_password.strip(), retrieved_password.strip()) & system_access:
         result = "Access Granted"
         print(result)
         # Retrieves the role permissions and access right from Control Mechanism
@@ -39,6 +45,12 @@ def login_verification(user_name, password_input, role, env):
         print(result + ' for ' + user_name + '. Access Denied')
         return result
 
+
+def password_is_valid(decoded_password, retrieved_password):
+    if decoded_password.strip() == retrieved_password.strip():
+        return True
+    else:
+        return False
 
 def read_password(user_name):
     password_file = open("password.txt")
@@ -63,4 +75,16 @@ def read_password(user_name):
     else:
         warnings.warn("User name not Found")
 
+def check_system_time_and_access():
+    now = datetime.datetime.now()
+    print('Current Time is ', now)
+
+    business_hours_open = now.replace(hour=9, minute=0, second=0, microsecond=0)
+    business_hours_close = now.replace(hour=17, minute=0, second=0, microsecond=0)
+
+    if business_hours_open < now < business_hours_close:
+        return True
+    else:
+        print("Teller is not allowed at this time to access the system")
+        return False
 login_main()
